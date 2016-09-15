9800a85eaf1e1cee09e10a6b739e470c45245ebd
Wang Cheng <u3003676@connect.hku.hk>
2016-09-15 14:40:29 +0800

    Update dare_server.c

--- src/dare_server.c
+++ src/dare_server.c
@@ -1508,7 +1508,7 @@ hb_timeout()
 static void
 start_election()
 {
-    if (data.config.idx != 0)
+    //if (data.config.idx != 0)
         //sleep(5); // leader does not sleep
     
     int rc, i;
