dfc424238920e2e7b059481a4cdf945af4406e44
Wang Cheng <u3003676@connect.hku.hk>
2016-09-17 21:31:22 +0800

    Update dare_ibv_ud.c

--- src/dare_ibv_ud.c
+++ src/dare_ibv_ud.c
@@ -2052,11 +2052,11 @@ int ud_create_clt_request()
             csm_req->cmd.len = bytes_read * sizeof(kvs_cmd_t);
             len += csm_req->cmd.len;
             
-            /*fprintf(log_fp, "\nTrace cmd: [%s] %s key=%s; data len=%"PRIu16"\n", 
+            fprintf(log_fp, "\nTrace cmd: [%s] %s key=%s; data len=%"PRIu16"\n", 
                 (hdr->type == CSM_READ) ? "READ" : "WRITE", 
                 (kvs_cmd->type == KVS_PUT) ? "PUT" : 
                 (kvs_cmd->type == KVS_GET) ? "GET" : "RM", 
-                kvs_cmd->key, kvs_cmd->len);*/
+                kvs_cmd->key, kvs_cmd->len);
             
             /* Read possible data */
             if (len + kvs_cmd->len > mtu_value(IBDEV->mtu)) {
