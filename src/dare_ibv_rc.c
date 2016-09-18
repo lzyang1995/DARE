e81daea5f3d9a1132df557b7a80cd563c23b386e
Wang Cheng <u3003676@connect.hku.hk>
2016-09-18 16:13:51 +0800

    Update dare_ibv_rc.c

--- src/dare_ibv_rc.c
+++ src/dare_ibv_rc.c
@@ -1665,7 +1665,7 @@ update_remote_logs()
                 //info_wtime(log_fp, "### Log update (%"PRIu64")\n", ssn);
                 init = 1;
             }
-            //TIMER_INFO(log_fp, "   (p%"PRIu8": write log[%"PRIu64":%"PRIu64"])\n", i, *remote_end, SRV_DATA->log->end);
+            TIMER_INFO(log_fp, "   (p%"PRIu8": write log[%"PRIu64":%"PRIu64"])\n", i, *remote_end, SRV_DATA->log->end);
             //info(log_fp, "   # (p%"PRIu8": write log[%"PRIu64":%"PRIu64"])\n", 
             //        i, *remote_end, SRV_DATA->log->end);
             /* Set remote offset */
@@ -1706,7 +1706,7 @@ update_remote_logs()
             /* Set the remote end offset to the cached end offset */
             remote_end = &SRV_DATA->ctrl_data->log_offsets[i].end;
             *remote_end = server->cached_end_offset;
-            //TIMER_INFO(log_fp, "   (p%"PRIu8".end=%"PRIu64")\n", i, *remote_end);
+            TIMER_INFO(log_fp, "   (p%"PRIu8".end=%"PRIu64")\n", i, *remote_end);
             //info(log_fp, "   # (p%"PRIu8
             //    ": update end offset to %"PRIu64")\n", i, *remote_end);
             /* Set send fields */
@@ -2046,7 +2046,7 @@ info(log_fp, "%s\n", buf);
             offset = (uint32_t) (offsetof(dare_log_t, commit));
             init = 1;
         }
-        //TIMER_INFO(log_fp, "   (p%"PRIu8".commit=%"PRIu64")\n", i, *remote_commit);
+        TIMER_INFO(log_fp, "   (p%"PRIu8".commit=%"PRIu64")\n", i, *remote_commit);
         
         /* Set remote offset */
         rm.raddr = ep->rc_ep.rmt_mr[LOG_QP].raddr + offset;
