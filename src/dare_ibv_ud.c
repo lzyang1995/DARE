41b82ae996e76613fc9d0cb3777d40156a81d1af
Wang Cheng <u3003676@connect.hku.hk>
2016-09-21 23:43:01 +0800

    Update dare_ibv_ud.c

--- src/dare_ibv_ud.c
+++ src/dare_ibv_ud.c
@@ -981,26 +981,6 @@ handle_csm_read_requests( struct ibv_wc *read_wcs, uint16_t read_count )
     //fprintf(lzyang_fp_ack, "rc_verify_leadership %llu\n", (long long unsigned int) diff);
     //HRT_GET_TIMESTAMP(SRV_DATA->t2);
 
-#ifdef HASH
-    struct timespec end_time;
-    clock_gettime(CLOCK_MONOTONIC, &end_time);
-    record_t l;
-    memset(&l, 0, sizeof(record_t));
-    for (i = 0; i < read_count; i++)
-    {
-    	record_t *p = NULL;
-    	client_req_t* req = (client_req_t*)(IBDEV->ud_recv_bufs[read_wcs[i].wr_id] + 40);
-    	l.key.client_id = req->hdr.clt_id;
-    	l.key.id = req->hdr.id;
-    	HASH_FIND(hh, records, &l.key, sizeof(record_key_t), p);
-    	if (p)
-    	{
-    		uint64_t diff = BILLION * (end_time.tv_sec - p->start_time.tv_sec) + end_time.tv_nsec - p->start_time.tv_nsec;
-    		fprintf(lzyang_fp_ack, "Normal %llu\n", (long long unsigned int) diff);
-    	}
-    }
-#endif
-
     for (i = 0; i < read_count; i++) {
         handle_one_csm_read_request(&read_wcs[i], 
             (client_req_t*)(IBDEV->ud_recv_bufs[read_wcs[i].wr_id] + 40));
@@ -1052,6 +1032,22 @@ handle_one_csm_read_request( struct ibv_wc *wc, client_req_t *request )
         //fprintf(log_fp, "There are not-committed write requests; so wait for %"PRIu64"\n", ep->wait_for_idx);
         memcpy(ep->last_read_request, request, wc->byte_len - 40);
         return;
+    }
+#endif
+
+#ifdef HASH
+    struct timespec end_time;
+    record_t l;
+    memset(&l, 0, sizeof(record_t));
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    record_t *p = NULL;
+    l.key.client_id = request->hdr.clt_id;
+    l.key.id = request->hdr.id;
+    HASH_FIND(hh, records, &l.key, sizeof(record_key_t), p);
+    if (p)
+    {
+    	uint64_t diff = BILLION * (end_time.tv_sec - p->start_time.tv_sec) + end_time.tv_nsec - p->start_time.tv_nsec;
+    	fprintf(lzyang_fp_ack, "Normal %llu\n", (long long unsigned int) diff);
     }
 #endif
 
